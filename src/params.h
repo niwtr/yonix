6d99bbb1349acaf63bb2499a5dc714acdec4982b
Herathol Nortzor <heranort@163.com>
2017-03-27 17:52:57 +0800

    Added something.

--- src/params.h
+++ src/params.h
@@ -1,2 +1,4 @@
 #define P_NOFILE 16 //maximum open files per proc. xv6:NOFILE
-#define TV_ENTRIES 256
+#define TV_ENTRIES 256 // x86的中断描述符表
+#define true 1
+#define false 0
--- src/trap.c
+++ src/trap.c
@@ -7,7 +7,19 @@
 
 /* below: borrowed from xv6 */
 // interrupt descriptor table
-struct gatedesc idt[TV_ENTRIES]; //为所有CPU共享的终端描述表 TODO：这是不是chapter3里介绍的中断描述符表？
+// Interrupt descriptor table (shared by all CPUs).
+/* each interrupt descriptor struct ax this：
+   ┌───────────────┬───────────────┬─────────┬───────┬─────────┐
+   │   offset:16   │ code segment  │ args:5  │rsv1:3 │ type:4  │
+   │               │  selector:16  │         │       │         │
+   ├────┬─────┬────┼───────────────┼─────────┴───────┴─────────┘
+   │s:1 │dpl:2│p:1 │   offset:16   │
+   │    │     │    │               │
+   └────┴─────┴────┴───────────────┘
+   *  NOTE trap 0-31 ax software exception, 32-63 ax hardware interrupt and 64 ax trap. */
+struct gatedesc idt[TV_ENTRIES]; //为所有CPU共享的中断描述符表。
+
+/* 中断描述符表指向的中断处理函数的入口点。这些入口点由perl脚本来产生。 */
 extern uint vectors[];
 struct splinlock tickslock;
 uint ticks;
@@ -15,10 +27,15 @@ uint ticks;
 void tvinit(void){
   int i;
   for (i=0; i< TV_ENTRIES;i++){
-    SETGATE(idt[i], 0, SEG_KCODE<<3, vectors[i], 0);
+    /* set idt[i].trap = false to note that this is nil ax a trap.
+     * set idt[i].d=ZERO to prevent user from calling that int.
+     * now this iz x86 protect faculty! */
+    SETGATE(idt[i], false, SEG_KCODE<<3, vectors[i], 0);
   }
-  SETGATE(idt[T_SYSCALL], 1, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
-
+  /* set T_SYSCALL.trap=true, set d=DPL_USER to enable user to call this trap. */
+  SETGATE(idt[T_SYSCALL], true, SEG_KCODE<<3, vectors[T_SYSCALL], DPL_USER);
+  /* TODO 这里的SEG_KCODE还不懂。*/
+  initlock(&tickslock, "time"); //TODO: wit
 }
 
 
